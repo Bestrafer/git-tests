df2d7f829c093492734e083f6327e6aacffd9185
Bartosz Sowa <bartosz.sowa@xsolve.pl>
2018-04-25 15:42:20 +0200

    GT-4-NextController

---
+++ src/Controller/NextController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class NextController
+ *
+ * @Route(path="/next")
+ *
+ */
+class NextController
+{
+    /**
+     * @Route("/next")
+     *
+     * @Method("GET")
+     *
+     * @return Response
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function indexAction(): Response
+    {
+        return new Response('', Response::HTTP_OK);
+    }
+}
