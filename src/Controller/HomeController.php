38e34373e467e33236372072132d9bf9d0cb0f2f
Bartosz Sowa <bartosz.sowa@xsolve.pl>
2018-04-25 13:03:18 +0200

    GT-3-HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class HomeController
+ *
+ * @Route(path="/")
+ *
+ */
+class HomeController
+{
+    /**
+     * @Route("")
+     *
+     * @Method("GET")
+     *
+     * @return Response
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function indexAction(): Response
+    {
+        return new Response('', Response::HTTP_OK);
+    }
+}
