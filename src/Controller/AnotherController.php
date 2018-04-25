828b2ef0ae3f8832c9ada8dd4da11dc0eb09f0bc
Bartosz Sowa <bartosz.sowa@xsolve.pl>
2018-04-25 15:45:02 +0200

    body

---
+++ src/Controller/AnotherController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class AnotherController
+ *
+ * @Route(path="/another")
+ *
+ */
+class AnotherController
+{
+    /**
+     * @Route("/another")
+     *
+     * @Method("GET")
+     *
+     * @return Response
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function indexAction(): Response
+    {
+        return new Response('', Response::HTTP_PARTIAL_CONTENT);
+    }
+}
