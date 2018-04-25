f1ad6b1a2946f628ff50fbff807ba01ca73cb022
Bartosz Sowa <bartosz.sowa@xsolve.pl>
2018-04-25 15:42:57 +0200

    GT-4-fix

--- src/Controller/NextController.php
+++ src/Controller/NextController.php
@@ -25,6 +25,6 @@ class NextController
      */
     public function indexAction(): Response
     {
-        return new Response('', Response::HTTP_OK);
+        return new Response('', Response::HTTP_PARTIAL_CONTENT);
     }
 }
